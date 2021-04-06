098bd77395bff9faa9b64180b0a9c9ced1e3d8bd
meight <meightpc@gmail.com>
2021-04-06 15:29:14 +0530

    first commit ;)

---
+++ src/Components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// function Greet() {
+//     return <h1>He!!0 me</h1>
+// }
+
+const Greet = () => <h1>He!!o from the other side</h1>
+
+export default Greet;
---
+++ src/Components/Welcome.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component{
+    render() {
+        return <h1>welcome from the class</h1>
+    }
+}
+
+export default Welcome;
